c140d7010d1e016733d25aa6a452ef9535187d0a
amit <amit@wpoets.com>
2014-12-14 20:15:31 +0530

    archive is now what was committed 2 rev back.. ie finally it supports post type, tax, cat and tag only.

--- archive.php
+++ archive.php
@@ -6,18 +6,41 @@
 get_header(); ?>
 <div class="container">
 	<div class="content row">
-		<main class="main col-sm-12 col-xs-12" role="main">
+		<main class="main col-md-12 col-xs-12" role="main">
 			<?php 
-			$tax = $wp_query->get_queried_object();
-			FIX rthidsa]ff
-			awesome2_library::setparam('default_taxonomy',$tax->taxonomy);
-			awesome2_library::setparam('default_term_slug',$tax->slug);
+			$shortcode ='[aw2_block slug="theme-archive"]';
 			awesome2_library::setparam('default_collection',$wp_query->posts);
+			if(is_post_type_archive( ))
+			{
+				$post_type = get_query_var('post_type');
+				if(awesome2_library::get_post_from_slug('theme-' . $post_type . '-archive','aw_block',$ignore))
+					$shortcode='[aw2_block slug="theme-' . $post_type . '-archive"]'; 
+			}
+			else if(is_tax())
+			{
+				$tax = $wp_query->get_queried_object();
+				awesome2_library::setparam('default_taxonomy',$tax->taxonomy);
+				awesome2_library::setparam('default_term_slug',$tax->slug);
+				
+				if(awesome2_library::get_post_from_slug('theme-' . $tax->taxonomy . '-archive','aw_block',$ignore))
+					$shortcode='[aw2_block slug="theme-' . $tax->taxonomy . '-archive"]'; 
 
- 			if(awesome2_library::get_post_from_slug('theme-' . $tax->taxonomy . '-archive','aw_block',$ignore))
-				echo do_shortcode('[aw2_block slug="theme-' . $tax->taxonomy . '-archive"]'); 
-			else
-				echo do_shortcode('[aw2_block slug="theme-archive"]');
+			}
+			else if(is_category()){
+				$cat = get_category( get_query_var( 'cat' ) );
+				awesome2_library::setparam('default_taxonomy','category');
+				awesome2_library::setparam('default_term_slug',$cat->slug);
+				if(awesome_library::get_post_from_slug('theme-category-' . $cat->slug . '-archive','aw_block',$ignore))
+					$shortcode='[aw2_block slug="theme-category-' . $cat->slug . '-archive"]'; 
+			}
+			else if( is_tag()){
+				//awesome2_library::setparam('default_tag',$wp_query->posts);
+				if(awesome_library::get_post_from_slug('theme-tag-' . $cat->slug . '-archive','aw_block',$ignore))
+					$shortcode='[aw2_block slug="theme-tag-' . $cat->slug . '-archive"]'; 
+			}
+
+			
+			echo do_shortcode($shortcode);
 			?>
 		</main><!-- /.main -->
 		<!-- <aside class="sidebar  col-sm-4 col-xs-12" role="complementary">
