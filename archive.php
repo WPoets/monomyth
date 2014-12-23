d7e846f4f0fd8b16cb3ab5e9213c14b965aa80da
amit <amit@wpoets.com>
2014-12-23 12:59:13 +0530

    added support for current name of archive term/tax/cat page

--- archive.php
+++ archive.php
@@ -13,6 +13,7 @@ get_header(); ?>
 			if(is_post_type_archive( ))
 			{
 				$post_type = get_query_var('post_type');
+				awesome2_library::setparam('current_archive_name',$post_type);
 				if(awesome2_library::get_post_from_slug('theme-' . $post_type . '-archive','aw_block',$ignore))
 					$shortcode='[aw2_block slug="theme-' . $post_type . '-archive"]'; 
 			}
@@ -21,7 +22,7 @@ get_header(); ?>
 				$tax = $wp_query->get_queried_object();
 				awesome2_library::setparam('default_taxonomy',$tax->taxonomy);
 				awesome2_library::setparam('default_term_slug',$tax->slug);
-				
+				awesome2_library::setparam('current_archive_name',$tax->name);
 				if(awesome2_library::get_post_from_slug('theme-' . $tax->taxonomy . '-archive','aw_block',$ignore))
 					$shortcode='[aw2_block slug="theme-' . $tax->taxonomy . '-archive"]'; 
 
@@ -30,11 +31,13 @@ get_header(); ?>
 				$cat = get_category( get_query_var( 'cat' ) );
 				awesome2_library::setparam('default_taxonomy','category');
 				awesome2_library::setparam('default_term_slug',$cat->slug);
+				awesome2_library::setparam('current_archive_name',$cat->name);
 				if(awesome_library::get_post_from_slug('theme-category-' . $cat->slug . '-archive','aw_block',$ignore))
 					$shortcode='[aw2_block slug="theme-category-' . $cat->slug . '-archive"]'; 
 			}
 			else if( is_tag()){
 				//awesome2_library::setparam('default_tag',$wp_query->posts);
+				awesome2_library::setparam('current_archive_name',$cat->name);
 				if(awesome_library::get_post_from_slug('theme-tag-' . $cat->slug . '-archive','aw_block',$ignore))
 					$shortcode='[aw2_block slug="theme-tag-' . $cat->slug . '-archive"]'; 
 			}
