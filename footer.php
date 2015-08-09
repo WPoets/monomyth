64e2c74c0310c1afa837e5fa06443ccb7b79b0a4
amit <amit@wpoets.com>
2015-08-09 09:01:26 +0530

    wrong footer was being used

--- footer.php
+++ footer.php
@@ -3,10 +3,10 @@
 if(isset($GLOBALS['aw2_footer']))
 	$local_footer=$GLOBALS['aw2_footer'];
 else
-	$local_header='footer';
+	$local_footer='footer';
 
 $footer_content=null;
-awesome2_library::get_post_content($aw2_footer,'aw2_core',$footer_content);
+awesome2_library::get_post_content($local_footer,'aw2_core',$footer_content);
 echo do_shortcode($footer_content);
  wp_footer(); 
  if (current_user_can('develop_for_awesomeui')) {
