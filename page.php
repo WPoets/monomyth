483cbc741665af97009ffb49c01f6c5259adfc4d
amit <amit@wpoets.com>
2016-05-20 19:09:42 +0530

    removed dummy codes.

--- page.php
+++ page.php
@@ -8,14 +8,16 @@ get_header(); ?>
 <div class="content row no-gutters">
 	<main class="main  col-lg-12 col-md-12 col-sm-12 col-xs-12" role="main">
 		<?php while ( have_posts() ) : the_post(); ?>
-
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<!-- <header class="entry-header">
 				<h1 class="entry-title"><?php the_title(); ?></h1>
 			</header> --><!-- .entry-header -->
 
 			<div class="entry-content">
-				<?php the_content(); ?>
+			
+				<?php 
+				the_content(); 
+				?>
 				<?php
 					wp_link_pages( array(
 						'before' => '<div class="page-links">' . __( 'Pages:', '_s' ),
