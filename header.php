2bc1338052e0a4a46b85c2bd24b0bc41f2d350d9
amit <amit@wpoets.com>
2016-06-28 16:46:15 +0530

    removed IE edge meta

--- header.php
+++ header.php
@@ -6,9 +6,8 @@
 <head>
 	<meta charset="utf-8">
 	<?php // Google Chrome Frame for IE ?>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no"/>
-	<title><?php wp_title(); ?></title>
 	<link rel='dns-prefetch' href='cdn.getawesomestudio.com'>
 	<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
 	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/apple-icon-touch.png">
@@ -24,23 +23,24 @@
 	<!--[if IE]>
 		<link rel="shortcut icon" href="<?php
 		echo aw2_library::get('site_settings.opt-favicon');
-	 ?>">	
+	 ?>">
+	<![endif]-->	 
 	<?php 	
 	}
-	?>
-	<![endif]-->
-	<?php // or, set /favicon.ico for IE10 win ?>
+	
+	// or, set /favicon.ico for IE10 win ?>
 	<meta name="msapplication-TileColor" content="#f01d4f">
 	<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/assets/images/win8-tile-icon.png">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 	<?php 
 	
+	wp_head();
+	
 	$jsdef=null;
-		
 	aw2_library::get_post_from_slug('global-definitions','aw2_core',$jsdef);	
 	echo aw2_library::parse_shortcode($jsdef->post_content);
 
-	wp_head(); ?>
+	?>
 </head>
 
 <body <?php body_class(); ?>>
